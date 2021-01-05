e0b8bc67bfbad2178cde25bd3ea201da53a7bd81
fabriciopatrocinio <fabricioschiffer@gmail.com>
2021-01-05 10:23:14 -0300

    Rotas add, porem nao funcionam

--- app/produtos.py
+++ app/produtos.py
@@ -1,7 +1,149 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for, request, redirect
+from app.serealizer import ProdutosSchema, UsersSchema
+from .model import Produtos, Users, Referencia, Fabricante, Tipo
+from flask_login import login_user, logout_user, login_required, current_user
 
-bp_produtos = Blueprint('produtos', __name__)
+bp_produtos = Blueprint('produtos', __name__, template_folder='templates')
 
-@bp_produtos.route('/login',  methods=['GET'])
-def login():
-    ...
+@bp_produtos.route('/', methods=['GET', 'POST'])
+def index():
+    title = 'Login'
+    
+    if request.method == 'POST':
+        nome = request.form['nome']
+        senha = request.form['senha']
+        
+        user = Users.query.filter_by(nome=nome).first()
+        
+        if not user or user.verificar_senha(senha):
+            return redirect(url_for('index'))
+        
+        login_user(user)
+        return  redirect(url_for('sistema-produtos'))
+    
+    return render_template('home.html', title=title)
+
+
+@bp_produtos.route('/criar-conta', methods=['GET', 'POST'])
+def criar_conta():
+    title = 'Criar Conta'
+    nome = ''
+    email = ''
+    senha = ''
+    erro = ''
+    msg = ''
+    
+    if request.method == 'POST':    
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+        
+        if nome != '' and email != '' and senha != '':
+            conta = Users(nome, email, senha)
+            db.session.add(conta)
+            db.session.commit()
+            
+            msg = 'Sua conta foi criada com sucesso!'
+        else:
+            erro = 'Você precisa preencher todos os dados.'
+            
+    return render_template('criar-conta.html', title=title, erro=erro, msg=msg)
+
+
+# Desfazer login e retorna a page login
+@bp_produtos.route('/logout', methods=['GET'])
+def logout():
+    logout_user()    
+    return redirect(url_for('index'))
+
+
+@bp_produtos.route('/admin/')
+def admin():
+    return '<h1>Admin</h1>'
+
+
+@bp_produtos.route('/sistema-produtos', methods=['GET', 'POST'])
+def sistema_produtos():
+    title = 'Sistema de produtos'
+    result = Produtos.query.all()
+    
+    return render_template('sistema-produtos.html', title=title, result=result)
+
+
+@bp_produtos.route('/adicionar-produtos', methods=['GET', 'POST'])
+def adicionar_produtos():
+    title = 'Adicionar produtos'
+    erro = ''
+    msg = ''
+    
+    # Isso gera a lista de categorias do <select> no HTML    
+    ref = Referencia.query.all()
+    fab = Fabricante.query.all()
+    tip = Tipo.query.all()
+    
+    
+    if request.method == 'POST':
+        cod = request.form['cod']
+        referencia = request.form['referencia']
+        descricao = request.form['descricao']
+        complemento = request.form['complemento']
+        fabricante = request.form['fabricante']
+        tipo = request.form['tipo']
+        quant = request.form['quant']
+        custo = request.form['custo']
+        preco = request.form['preco']
+        
+        produtos = Produtos(cod, referencia, descricao, complemento, fabricante, tipo, quant, custo, preco)
+        
+        db.session.add(produtos)
+        db.session.commit()
+        
+        msg = 'Produto adicionado com sucesso'
+    else:
+        if request.method == 'POST':
+            erro = 'Erro ao adicionar produtos, por favor insira todos os dados corretamente.'
+    
+    return render_template('adicionar-produtos.html', title=title, msg=msg, erro=erro, ref=ref, fab=fab, tip=tip)
+
+
+@bp_produtos.route('/adicionar-categorias', methods=['GET', 'POST'])
+def adicionar_categorias():
+    title = 'Adicionar categorias'
+    erro = ''
+    msg = ('')
+    
+    if request.method == 'POST' and request.form['referencia'] != '':
+        referencia = request.form['referencia']
+        ref = Referencia(referencia)
+        
+        db.session.add(ref)
+        db.session.commit()
+        msg = 'Categoria(s) adicionada(s) com sucesso!'
+
+    if request.method == 'POST' and request.form['fabricante'] != '':
+        fabricante = request.form['fabricante']
+        fab = Fabricante(fabricante)
+        
+        db.session.add(fab)
+        db.session.commit()
+        msg = 'Categoria(s) adicionada(s) com sucesso!'
+
+    if request.method == 'POST' and request.form['tipo'] != '':
+        tipo = request.form['tipo']
+        tip = Tipo(tipo)
+        
+        db.session.add(tip)
+        db.session.commit()
+        msg = 'Categoria(s) adicionada(s) com sucesso!'
+
+    return render_template('adicionar-categorias.html', title=title, erro=erro, msg=msg)
+
+
+@bp_produtos.route('/produtos-em-falta', methods=['GET', 'POST'])
+def produtos_em_falta():
+    title = 'Produtos em falta'
+    
+    # Pega produtos com quantidade igual a ZERO
+    result = Produtos.query.filter(db.or_((Produtos.quant <= 2))).all()
+    
+    return render_template('produtos-em-falta.html', title=title, result=result)
--- app/serealizer.py
+++ app/serealizer.py
@@ -1,12 +1,27 @@
 from flask_marshmallow import Marshmallow
-from .model import Produtos
+from .model import Produtos, Users, Referencia, Fabricante, Tipo
 
 ma = Marshmallow()
 
 def configure(app):
     ma.init_app(app)
 
-
 class ProdutosSchema(ma.Schema):
     class Meta:
         model = Produtos
+
+class UsersSchema(ma.Schema):
+    class Meta:
+        model = Users
+
+class ReferenciaSchema(ma.Schema):
+    class Meta:
+        model = Referencia
+
+class FabricanteSchema(ma.Schema):
+    class Meta:
+        model = Fabricante
+
+class TipoSchema(ma.Schema):
+    class Meta:
+        model = Tipo
