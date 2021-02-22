df3fdf4abddecfb5c64d22cb0b21609d0005f2f6
fabriciopatrocinio <fabricioschiffer@gmail.com>
2021-02-22 14:03:08 -0300

    Add contas a receber, cerreçao de bugs

--- app/financeiro/views.py
+++ app/financeiro/views.py
@@ -1,15 +1,15 @@
 from flask import render_template, url_for, flash, request, redirect
+from flask_login import login_user, logout_user, login_required, current_user
 from sqlalchemy import exc
 from . import bp_financeiro
 from operator import length_hint
-from flask_login import login_user, logout_user, login_required, current_user
-from app.model import db
-from app.model import Users, Contas, Fiado
+from app.model import db, Users, Contas, Fiado
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import func
 from datetime import date
 
 
+# Contas a pagar
 @bp_financeiro.route('/contas-a-pagar/', methods=['GET', 'POST'])
 def contas_a_pagar():
     title = 'Contas a pagar'
@@ -71,11 +71,64 @@ def contas_situacao(id_conta, situacao):
     return redirect(url_for('bp_financeiro.contas_a_pagar'))
 
 
+# =================================================================
+# Contas a receber
 @bp_financeiro.route('/contas-a-receber', methods=['GET', 'POST'])
 def contas_a_receber():
     title = 'Contas a receber'
+    hoje = date.today().strftime('%Y-%m-%d')
     
     user_id = current_user.id
     user = Users.query.filter_by(id=user_id).first()
     
     fiado = Fiado.query.filter_by(user_id=user_id).all()
+    
+    if request.method == 'POST' and request.form['nome'] != '' and request.form['data_conta'] != '' and request.form['valor'] != '':
+        user_id = request.form['user']
+        nome = request.form['nome'] 
+        complemento = request.form['complemento']
+        data_conta = request.form['data_conta']
+        valor = request.form['valor']
+        situacao = int(request.form['situacao'])
+
+        try:
+            fiado = Fiado(user_id, nome, complemento, data_conta, valor, situacao)
+            
+            db.session.add(fiado)
+            db.session.commit()
+            
+            flash('Conta adicionada com sucesso', 'success')
+        except IntegrityError:
+            db.session.rollback()
+            flash('Ocorreu algum erro ao adicionar sua conta, por favor tente novamente.')
+    else:
+        if request.method == 'POST':
+            flash('Erro ao adicionar conta, por favor insira todos os dados corretamente.', 'danger')
+    
+    return render_template('contas-a-receber.html', title=title, fiado=fiado, user=user, hoje=hoje)
+
+
+@bp_financeiro.route('/contas-a-receber/filtrar/', methods=['GET', 'POST'])
+def fiado_filtrar():
+    title = 'Contas a receber filtrar'
+    hoje = date.today().strftime('%Y-%m-%d')
+    
+    user_id = current_user.id
+    user = Users.query.filter_by(id=user_id).first()
+    
+    if request.method == 'POST' and request.form['data_filtro'] != '':
+        data_filtro = request.form['data_filtro']
+        fiado = Fiado.query.filter(func.DATE(Fiado.data_conta) == data_filtro).filter_by(user_id=user.id)
+    
+    return render_template('contas-a-receber.html', title=title, fiado=fiado, user=user, hoje=hoje)
+
+
+@bp_financeiro.route('/conta-a-receber/situacao/<int:id_conta>/<int:situacao>', methods=['GET', 'POST'])
+def fiado_situacao(id_conta, situacao):
+    
+    result = Fiado.query.filter_by(id=id_conta).first()
+    
+    result.situacao = situacao
+    db.session.commit()
+    
+    return redirect(url_for('bp_financeiro.contas_a_receber'))
